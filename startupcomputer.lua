f58d0ce6203297279109dce36e0eae4eaa4ef8a4
Kazu <68849644+kazoothetoot@users.noreply.github.com>
2025-04-16 19:23:41 +1200

    yoza

---
+++ startupcomputer.lua
@@ -0,0 +1,70 @@
+-- setup
+local mon = peripheral.find("monitor") or error("No monitor found")
+local modem = peripheral.find("modem") or error("No modem found")
+rednet.open(peripheral.getName(modem))
+
+
+mon.setTextScale(1)
+mon.setBackgroundColor(colors.black)
+
+
+function drawUI()
+    mon.clear()
+  
+    mon.setCursorPos(1, 1)
+    mon.setBackgroundColor(colors.green)
+    mon.setTextColor(colors.white)
+    mon.write("Move Forward")
+  
+    mon.setCursorPos(1, 2)
+    mon.setBackgroundColor(colors.orange)
+    mon.setTextColor(colors.white)
+    mon.write("Start Mining")
+  
+    mon.setCursorPos(1, 3)
+    mon.setBackgroundColor(colors.orange)
+    mon.setTextColor(colors.yellow)
+    mon.write("Fuel Check")
+  
+    mon.setCursorPos(1, 4)
+    mon.setBackgroundColor(colors.red)
+    mon.setTextColor(colors.white)
+    mon.write("Exit")
+end
+
+local function isInBox(x, y, x1, y1, x2, y2)
+    return x >= x1 and x <= x2 and y >= y1 and y <= y2
+end
+
+while true do
+    drawUI()
+    local _, _, x, y = os.pullEvent("monitor_touch")
+
+    if isInBox(x, y, 1, 1, 10, 1) then
+        rednet.broadcast("moveforward")
+        mon.setCursorPos(1, 6)
+        mon.write("Moving Forward")
+    elseif isInBox(x, y, 1, 2, 12, 2) then
+        rednet.broadcast("mine", "turtle")
+        mon.setCursorPos(1, 6)
+        mon.write("Started Mining...")
+    elseif isInBox(x,y,1,3,18,3) then
+        rednet.broadcast("fuel", "turtle")
+        mon.setCursorPos(1,6)
+        mon.write("Fuel Check Pressed")
+        local id, reply = rednet.receive("computer", 3)
+        mon.setCursorPos(1, 7)
+        if reply then
+            mon.write("Fuel level: " .. reply)
+        else
+            mon.write("No Response.")
+        end
+    elseif isInBox(x, y, 1, 4, 18, 4) then
+        rednet.broadcast("exit", "turtle")
+        mon.setCursorPos(1,6)
+        mon.write("Exiting...")
+        os.sleep(1)
+        term.setCursorPos(1,1)
+        term.error()
+    end
+end
